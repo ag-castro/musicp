a82ea6782d7dbeb19e48ceb715bcc8760919dd59
webappsagency <agcastro.py@yahoo.com>
2019-09-20 21:08:13 -0300

    CoreModule basic setup

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+
+  // Verifica se o CoreModule é carregado apenas em AppModule.
+  constructor(
+    @Optional() @SkipSelf() parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule, já foi carregado. Importe o CoreModule apenas no AppModule.');
+    }
+  }
+
+ }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  MatButtonModule, MatCardModule, MatIconModule,
+  MatListModule, MatToolbarModule, MatMenuModule } from '@angular/material';
+import { HeaderToolBarComponent } from './components/header-tool-bar/header-tool-bar.component';
+import { NoRecordComponent } from './components/no-record/no-record.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderToolBarComponent, NoRecordComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatMenuModule,
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatMenuModule,
+
+    HeaderToolBarComponent,
+    NoRecordComponent
+  ],
+
 })
 export class SharedModule { }
