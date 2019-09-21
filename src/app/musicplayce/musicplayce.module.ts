9e323675fd191d339137c1b80239b02a2c15a0f5
webappsagency <agcastro.py@yahoo.com>
2019-09-20 21:04:28 -0300

    Added basics modules

---
+++ src/app/musicplayce/musicplayce.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MusicplayceRoutingModule } from './musicplayce-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MusicplayceRoutingModule
+  ]
+})
+export class MusicplayceModule { }
