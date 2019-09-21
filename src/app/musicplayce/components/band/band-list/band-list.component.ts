b4a12a178a85e2bd946f404484117f4a5126adfa
webappsagency <agcastro.py@yahoo.com>
2019-09-20 21:29:10 -0300

    Band components

---
+++ src/app/musicplayce/components/band/band-list/band-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-band-list',
+  templateUrl: './band-list.component.html',
+  styleUrls: ['./band-list.component.scss']
+})
+export class BandListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/musicplayce/musicplayce.module.ts
+++ src/app/musicplayce/musicplayce.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { MusicplayceRoutingModule } from './musicplayce-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
+import { BandFormComponent } from './components/band/band-form/band-form.component';
+import { BandListComponent } from './components/band/band-list/band-list.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    BandFormComponent,
+    BandListComponent,
   ],
   imports: [
     SharedModule,
